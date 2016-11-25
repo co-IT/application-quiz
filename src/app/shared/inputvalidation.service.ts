b75c4278a6b0c05e1326a56c13fc13ef78e90593
admin <mail@strieter.eu>
2016-11-25 11:37:18 +0100

    allow upper case letters in emailvalidation

--- src/app/shared/inputvalidation.service.ts
+++ src/app/shared/inputvalidation.service.ts
@@ -15,7 +15,7 @@ export class InputvalidationService {
          
     static emailValidator(control) {
         // RFC 2822 compliant regex
-        if (control.value.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)) {
+        if (control.value.match(/[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
             return null;
         } else {
             return { 'invalidEmailAddress': true };
