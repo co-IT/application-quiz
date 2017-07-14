62feeab6a7c4c94efe380a7f0d579f9bb77668f0
Frank Strieter <fs@co-it.eu>
2017-07-14 12:07:56 +0200

    adds type for attendee

--- src/app/shared/attendee.ts
+++ src/app/shared/attendee.ts
@@ -1,5 +1,6 @@
 export class Attendee {
   constructor(public firstname: string,
               public lastname: string,
-              public email: string) { }
+              public email: string,
+              public type: string) { }
 }
