1d5a4bc51fe5b928fc5efaa3bbd4926fbb076a2b
Frank Strieter <fs@co-it.eu>
2017-07-18 07:33:04 +0200

    allows uppercase in email-address

--- src/app/shared/inputvalidation.service.ts
+++ src/app/shared/inputvalidation.service.ts
@@ -5,29 +5,20 @@ export class InputvalidationService {
 
   static getValidatorErrorMessage(validatorName: string, validatorValue?: any) {
         let config = {
-            'required': 'Erforderliches Feld',       
+            'required': 'Erforderliches Feld',
             'invalidEmailAddress': 'Keine gültige Email-Adesse',
             'minlength': 'Mindestens 2 Zeichen'
         };
         return config[validatorName];
     }
 
-         
     static emailValidator(control) {
         // RFC 2822 compliant regex
-        if (control.value.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)) {
+        if (control.value
+          .match(/[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?/)) {
             return null;
         } else {
             return { 'invalidEmailAddress': true };
         }
     }
-    
-    
-     
-   
-
-    
-    
-    
-    
 }
